f892c37b326d55ff18b10cc7fef44e351cafafdf
Margherita Fiorillo <margheritafiorillo@MacBook-Pro-di-Margherita.local>
2026-02-16 13:09:01 +0100

    pulizia cartelle

--- backend/routes/tournaments.php
+++ backend/routes/tournaments.php
@@ -4,4 +4,4 @@ use App\Controllers\TournamentController;
 
 // Rotte per la risorsa Tournaments
 Router::get('/tournaments', 'TournamentController@index');  // Lista tornei
-Router::post('/tournaments', 'TournamentController@store'); // Crea un torneo (e genera il tabellone)
+Router::post('/tournaments', 'TournamentController@store'); // Crea un torneo e genera il tabellone
